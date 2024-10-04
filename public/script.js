f2c6e6dd8274bbfcefaa4c6518c827faf9d652f2
illustr <still.lkmn@gmail.com>
2024-10-04 22:21:32 +0700

    minor adjustment

--- public/script.js
+++ public/script.js
@@ -1,13 +1,9 @@
-// Select the theme toggle checkbox
 const themeToggle = document.getElementById('theme-toggle');
 
-// Check for saved theme in localStorage
 if (localStorage.getItem('theme') === 'light') {
     document.body.classList.add('light');
     themeToggle.checked = true;
 }
-
-// Add event listener to the theme toggle
 themeToggle.addEventListener('change', () => {
     if (themeToggle.checked) {
         document.body.classList.add('light');
